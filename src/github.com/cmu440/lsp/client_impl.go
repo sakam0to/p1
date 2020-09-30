b546080058a88752386954fd89fbdaa9b71d52f1
Michael Brzozowski <mbrzozowski@gatech.edu>
2020-09-30 02:23:39 -0400

    Tabs to spaces

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -3,25 +3,25 @@
 package lsp
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/cmu440/lspnet"
+    "encoding/json"
+    "fmt"
+    "github.com/cmu440/lspnet"
 )
 
 type client struct {
-	conn         *lspnet.UDPConn
-	connID       int
-	seq          int
-	unackedCount int
-	params       *Params
-	writeChan    chan []byte
-	readChan     chan []byte
-	setUnackChan chan int
-	getUnackChan chan bool
-	retUnackChan chan int
-	closeMain    chan bool
-	closeWrite   chan bool
-	closeRead    chan bool
+    conn         *lspnet.UDPConn
+    connID       int
+    seq          int
+    unackedCount int
+    params       *Params
+    writeChan    chan []byte
+    readChan     chan []byte
+    setUnackChan chan int
+    getUnackChan chan bool
+    retUnackChan chan int
+    closeMain    chan bool
+    closeWrite   chan bool
+    closeRead    chan bool
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -35,130 +35,130 @@ type client struct {
 // hostport is a colon-separated string identifying the server's host address
 // and port number (i.e., "localhost:9999").
 func NewClient(hostport string, params *Params) (Client, error) {
-	raddr, _ := lspnet.ResolveUDPAddr("udp", hostport)
-	conn, _ := lspnet.DialUDP("udp", nil, raddr)
-	msg := NewConnect()
-	var b []byte = make([]byte, 1024)
-	var b_ []byte = make([]byte, 1024)
-	b, _ = json.Marshal(msg)
-	_, _ = conn.Write(b)
-	for {
-		// Eventually some epoch logic here?
-		// Currently assuming the ack is going to arrive eventually
-		// Will need to get the connect requesting write from above in the loop accordingly
-		var rcvMsg Message
-		n, err := conn.Read(b_)
-		err = json.Unmarshal(b_[0:n], &rcvMsg)
-		if rcvMsg.Type == MsgAck {
-			client := &client{
-				conn:         conn,
-				connID:       rcvMsg.ConnID,
-				seq:          1,
-				unackedCount: 0,
-				params:       params,
-				writeChan:    make(chan []byte),
-				readChan:     make(chan []byte),
-				setUnackChan: make(chan int),
-				getUnackChan: make(chan bool),
-				retUnackChan: make(chan int),
-				closeMain:    make(chan bool),
-				closeWrite:   make(chan bool),
-				closeRead:    make(chan bool),
-			}
-			go client.MainRoutine()
-			go client.ReadRoutine()
-			go client.WriteRoutine()
-			return client, err
-		} else {
-			fmt.Printf("Connect received: %s\n", string(b_))
-			err = json.Unmarshal(b_, &rcvMsg)
-			if err != nil {
-				fmt.Println("Unmarshal error", err)
-			} else {
-				println(rcvMsg.Type)
-				println(rcvMsg.ConnID)
-			}
-		}
-	}
+    raddr, _ := lspnet.ResolveUDPAddr("udp", hostport)
+    conn, _ := lspnet.DialUDP("udp", nil, raddr)
+    msg := NewConnect()
+    var b []byte = make([]byte, 1024)
+    var b_ []byte = make([]byte, 1024)
+    b, _ = json.Marshal(msg)
+    _, _ = conn.Write(b)
+    for {
+        // Eventually some epoch logic here?
+        // Currently assuming the ack is going to arrive eventually
+        // Will need to get the connect requesting write from above in the loop accordingly
+        var rcvMsg Message
+        n, err := conn.Read(b_)
+        err = json.Unmarshal(b_[0:n], &rcvMsg)
+        if rcvMsg.Type == MsgAck {
+            client := &client{
+                conn:         conn,
+                connID:       rcvMsg.ConnID,
+                seq:          1,
+                unackedCount: 0,
+                params:       params,
+                writeChan:    make(chan []byte),
+                readChan:     make(chan []byte),
+                setUnackChan: make(chan int),
+                getUnackChan: make(chan bool),
+                retUnackChan: make(chan int),
+                closeMain:    make(chan bool),
+                closeWrite:   make(chan bool),
+                closeRead:    make(chan bool),
+            }
+            go client.MainRoutine()
+            go client.ReadRoutine()
+            go client.WriteRoutine()
+            return client, err
+        } else {
+            fmt.Printf("Connect received: %s\n", string(b_))
+            err = json.Unmarshal(b_, &rcvMsg)
+            if err != nil {
+                fmt.Println("Unmarshal error", err)
+            } else {
+                println(rcvMsg.Type)
+                println(rcvMsg.ConnID)
+            }
+        }
+    }
 }
 
 func (c *client) MainRoutine() {
-	for {
-		select {
-		case <-c.closeMain:
-			return
-		case delta := <-c.setUnackChan:
-			c.unackedCount += delta
-		case <- c.getUnackChan:
-			c.retUnackChan <- c.unackedCount
-		}
-	}
+    for {
+        select {
+        case <-c.closeMain:
+            return
+        case delta := <-c.setUnackChan:
+            c.unackedCount += delta
+        case <- c.getUnackChan:
+            c.retUnackChan <- c.unackedCount
+        }
+    }
 }
 
 func (c *client) ReadRoutine() {
-	for {
-		select {
-		case <-c.closeRead:
-			return
-		default:
-			var rcvMsg Message
-			var b []byte = make([]byte, 1024)
-			// Todo: Capture error handling in the new routines to send out through channel struct field
-			n, _ := c.conn.Read(b)
-			_ = json.Unmarshal(b[0:n], &rcvMsg)
-			if rcvMsg.Type == MsgData {
-				ack := NewAck(c.connID, rcvMsg.SeqNum)
-				b, _ = json.Marshal(ack)
-				_, _ = c.conn.Write(b)
-				c.readChan <- rcvMsg.Payload
-			} else if rcvMsg.Type == MsgAck {
-				c.setUnackChan <- -1
-			}
-		}
-	}
+    for {
+        select {
+        case <-c.closeRead:
+            return
+        default:
+            var rcvMsg Message
+            var b []byte = make([]byte, 1024)
+            // Todo: Capture error handling in the new routines to send out through channel struct field
+            n, _ := c.conn.Read(b)
+            _ = json.Unmarshal(b[0:n], &rcvMsg)
+            if rcvMsg.Type == MsgData {
+                ack := NewAck(c.connID, rcvMsg.SeqNum)
+                b, _ = json.Marshal(ack)
+                _, _ = c.conn.Write(b)
+                c.readChan <- rcvMsg.Payload
+            } else if rcvMsg.Type == MsgAck {
+                c.setUnackChan <- -1
+            }
+        }
+    }
 }
 
 func (c *client) WriteRoutine() {
-	for {
-		select {
-		case <-c.closeWrite:
-			return
-		default:
-			payload := <-c.writeChan
-			checksum := ByteArray2Checksum(payload)
-			msg := NewData(c.connID, c.seq, len(payload), payload, uint16(checksum))
-			b, _ := json.Marshal(msg)
-			for {
-				c.getUnackChan <- true
-				unackedCount := <-c.retUnackChan
-				if unackedCount < c.params.MaxUnackedMessages {
-					_, _ = c.conn.Write(b)
-					c.setUnackChan <- 1
-					break
-				}
-			}
-			c.seq++
-		}
-	}
+    for {
+        select {
+        case <-c.closeWrite:
+            return
+        default:
+            payload := <-c.writeChan
+            checksum := ByteArray2Checksum(payload)
+            msg := NewData(c.connID, c.seq, len(payload), payload, uint16(checksum))
+            b, _ := json.Marshal(msg)
+            for {
+                c.getUnackChan <- true
+                unackedCount := <-c.retUnackChan
+                if unackedCount < c.params.MaxUnackedMessages {
+                    _, _ = c.conn.Write(b)
+                    c.setUnackChan <- 1
+                    break
+                }
+            }
+            c.seq++
+        }
+    }
 }
 
 func (c *client) ConnID() int {
-	return c.connID
+    return c.connID
 }
 
 func (c *client) Read() ([]byte, error) {
-	read := <-c.readChan
-	return read, nil
+    read := <-c.readChan
+    return read, nil
 }
 
 func (c *client) Write(payload []byte) error {
-	c.writeChan <- payload
-	return nil
+    c.writeChan <- payload
+    return nil
 }
 
 func (c *client) Close() error {
-	c.closeMain <- true
-	c.closeRead <- true
-	c.closeWrite <- true
-	return nil
+    c.closeMain <- true
+    c.closeRead <- true
+    c.closeWrite <- true
+    return nil
 }
